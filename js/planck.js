92a4f0e6feb55e3be637753fe97f9bd2d76ec57e
nemoNoboru <felipetavres@gmail.com>
2017-02-03 16:48:21 +0100

    basic behaviour done

--- js/planck.js
+++ js/planck.js
@@ -3,10 +3,40 @@ var app = angular.module('plank',['ngAnimate'])
 app.controller('plankCtrl',['$scope',function ($scope) {
   $scope.stack = []
   $scope.display = ''
+  this.quick = false
 
-  this.add = function (n) {
-    console.log('troll');
-    $scope.display += n
+  this.sqrt = function () {
+    if($scope.display && !this.quick){
+      var x = parseInt($scope.display)
+      this.done(Math.sqrt(x))
+    }else{
+      var x = $scope.stack.pop()
+      this.done(Math.sqrt(x))
+    }
+  }
+
+  this.sum = function () {
+    this.feed(function (op1,op2) {
+      return op1 + op2
+    })
+  }
+
+  this.mul = function () {
+    this.feed(function (op1,op2) {
+      return op1 * op2
+    })
+  }
+
+  this.minus = function () {
+    this.feed(function (op1,op2) {
+      return op1 - op2
+    })
+  }
+
+  this.div = function () {
+    this.feed(function (op1,op2) {
+      return op1 / op2
+    })
   }
 
   this.push = function () {
@@ -14,10 +44,70 @@ app.controller('plankCtrl',['$scope',function ($scope) {
     $scope.display = ''
   }
 
-  this.sum = function () {
+  this.drop = function () {
+    result = $scope.stack.pop()
+    $scope.display = result
+    this.quick = true
+  }
+
+  this.add = function (n) {
+    if(this.quick){
+      $scope.display = ''
+      this.quick = false
+    }
+    $scope.display += n
+  }
+
+  this.remove = function () {
+    if(this.quick){
+      $scope.display = ''
+      this.quick = false
+    }
+    $scope.display = $scope.display.slice(0, -1);
+  }
+
+  this.done = function (result) {
+    $scope.display = result
+    this.quick = true
+    $scope.stack.push(result)
+  }
+
+  this.feed = function (callback) {
     var n = $scope.stack.pop()
-    var n1 = $scope.stack.pop()
-    $scope.stack.push(n+n1)
-    $scope.display = n+n1
+    if($scope.display && !this.quick){
+      var x = parseInt($scope.display)
+      this.done(callback(x,n))
+    }else{
+      var x = $scope.stack.pop()
+      this.done(callback(n,x))
+    }
+  }
+
+  this.key = function (e) {
+    console.log(e.keyCode);
+    if(e.keyCode <= 57 && e.keyCode >= 48){
+      this.add(e.key)
+    }
+    if(e.key === '-'){
+      this.minus()
+    }
+    if(e.key === '+'){
+      this.sum()
+    }
+    if(e.key === '/'){
+      this.div()
+    }
+    if(e.key === '*'){
+      this.mul()
+    }
+    if(e.keyCode === 13){
+      this.push()
+    }
+    if(e.keyCode === 8){
+      this.remove()
+    }
+    if(e.keyCode === 190){
+      this.drop()
+    }
   }
 }])
